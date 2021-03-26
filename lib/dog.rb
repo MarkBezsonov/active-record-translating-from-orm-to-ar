f020df16064679037ef3652b6395c1431c7b74f6
MarkBezsonov <markbezsonov@gmail.com>
2021-03-25 21:53:02 -0500

    commit tree 05fef4533597793fdcafa4c6226fe1afbbb69731

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,9 @@
-class Dog
+class Dog < ActiveRecord::Base
+    sql = <<-SQL
+        CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT
+        breed TEXT
+        )
+    SQL
 end
